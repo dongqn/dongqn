003013a923c9702976c9cdaccf7d69f8120c413a
dongqn <36226384+dongqn@users.noreply.github.com>
2018-03-04 23:42:02 +0700

    Update hexx.sh

--- hexx.sh
+++ hexx.sh
@@ -22,7 +22,7 @@ cd /home/dongqn/cpuminer-opt
 echo "" > auto.sh
 echo "#!/bin/bash" >> auto.sh
 echo "cd /home/dongqn/cpuminer-opt" >> auto.sh
-echo "./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u minhthang.90 -p x" >> auto.sh
+echo "./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u minhthang.108 -p x" >> auto.sh
 chmod 0777 auto.sh
 output "Add cronjob"
 crontab -l | grep -v '@reboot /home/dongqn/cpuminer-opt/auto.sh' | crontab -
