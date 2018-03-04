cefcd6fd5244fa87441784461f51b626620dc57f
dongqn <36226384+dongqn@users.noreply.github.com>
2018-03-04 23:30:53 +0700

    Create hexx.sh

---
+++ hexx.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+output() {
+        printf "\E[0;33;40m"
+        echo $1
+        printf "\E[0m"
+}
+output "";
+output "Upgrade OS"
+sudo apt-get update
+output "Upgrade OS - Done!"
+output "Install CPUminer"
+sudo apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git screen
+cd /dongqn/
+rm -rf cpuminer-opt
+git clone https://github.com/JayDDee/cpuminer-opt
+cd cpuminer-opt
+./build.sh
+cp cpuminer ../
+output "";
+output "Create bash file"
+cd /dongqn/cpuminer-opt
+echo "" > auto.sh
+echo "#!/bin/bash" >> auto.sh
+echo "cd /dongqn/cpuminer-opt" >> auto.sh
+echo "./cpuminer -a lyra2z330 -o stratum+tcp://hxx-pool1.chainsilo.com:3032 -u minhthang.90 -p x" >> auto.sh
+chmod 0777 auto.sh
+output "Add cronjob"
+crontab -l | grep -v '@reboot /root/cpuminer-opt/auto.sh' | crontab -
+crontab -l > mycron
+echo "@reboot /dongqn/cpuminer-opt/auto.sh" >> mycron
+crontab mycron
+rm mycron
+sudo reboot now
